1fa942d9b7b37633276f50ebb98c61f65c96128d
Iurii Skoliar <iurii.work@gmail.com>
2020-07-28 14:20:15 -0700

    File monitor project.

---
+++ FileMonitor/Sources/ApplicationDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  ApplicationDelegate.swift
+//  FileMonitor
+//
+//  Created by Iurii Skoliar on 7/28/20.
+//  Copyright © 2020 Iurii Skoliar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class ApplicationDelegate: NSObject, NSApplicationDelegate {
+}
