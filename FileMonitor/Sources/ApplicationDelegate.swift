5d726fad8e1ed69a7bd1f30722b9b5aa3bbf27c5
Iurii Skoliar <iurii.work@gmail.com>
2020-07-28 14:22:47 -0700

    'Open…' menu item.

--- FileMonitor/Sources/ApplicationDelegate.swift
+++ FileMonitor/Sources/ApplicationDelegate.swift
@@ -9,5 +9,20 @@
 import Cocoa
 
 @NSApplicationMain
-class ApplicationDelegate: NSObject, NSApplicationDelegate {
+class ApplicationDelegate: NSObject {
+	@IBAction func openFile(_ sender: Any) {
+		let openPanel = NSOpenPanel()
+		openPanel.canChooseDirectories = true
+		openPanel.directoryURL = FileManager.default.homeDirectoryForCurrentUser
+		if openPanel.runModal() == .OK, let fileURL = openPanel.url {
+			print("TODO: Open file (\(fileURL.path))")
+		}
+	}
+}
+
+extension ApplicationDelegate: NSApplicationDelegate {
+	func applicationShouldOpenUntitledFile(_ application: NSApplication) -> Bool {
+		self.openFile(self)
+		return false
+	}
 }
